91bedba2672762c57abd9f59039e4480a8bd3087
Eng-Mohamed <mkayta2023@gmail.com>
2024-09-22 18:58:30 +0300

    I done this challenge

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Good Luck 💪🏾
+function getFirstNames(array){
+    return array.map((name)=> name[0]);
+   
+}
+ let names=[["Ahmed","Aisha","Amina"],["Jaamac","Khadijah","Layla"],["Mohamed","Ali"]]
+ const result= getFirstNames(names)
+ console.log(result)
